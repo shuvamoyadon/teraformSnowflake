45169ee66d0257ba4693aee06119c926a55c777f
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2023-07-14 12:39:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "snowflake" {
+}
+
+resource "snowflake_database" "example" {
+  name = "EXAMPLE_DATABASE"
+  comment = "A database created by terraform"
+}
+
+resource "snowflake_warehouse" "example" {
+  name = "EXAMPLE_WAREHOUSE"
+  warehouse_size = "X-SMALL"
+  auto_suspend = 60
+  auto_resume = false
+  initially_suspended = true
+}
