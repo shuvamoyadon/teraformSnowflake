512903d50e18d75e9481b533e3144ebc072fc534
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2023-07-14 17:07:41 +0530

    gjg
    jj

--- actions/terraform-snowflake/main.tf
+++ actions/terraform-snowflake/main.tf
@@ -1,29 +1,24 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.61"
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "myteraformcloud"
+
+    workspaces {
+      name = "teraformSnowflake"
     }
   }
 }
 
 provider "snowflake" {
-  account  = "eg42130"
-  region   = "us-east-2.aws"
-  username = "ADITI007"
-  password = "Iamhere1"
-  role     = "ACCOUNTADMIN"
 }
 
-resource "snowflake_database" "example" {
-  name = "mydb"
-  comment = "A database created by terraform"
-}
-
-resource "snowflake_warehouse" "example" {
-  name = "COMPUTE_WH"
-  warehouse_size = "SMALL"
-  auto_suspend = 70
-  auto_resume = false
-  initially_suspended = true
+resource "snowflake_database" "demo_db" {
+  name    = "mydb"
+  comment = "Database for Snowflake Terraform demo"
 }
