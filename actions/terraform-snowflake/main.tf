75c9ba307b19424ec5403f918129094268f90100
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2023-07-14 15:12:06 +0530

    test
    test

--- actions/terraform-snowflake/main.tf
+++ actions/terraform-snowflake/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.61"
+    }
+  }
+}
+
 provider "snowflake" {
   role     =  "ACCOUNTADMIN"
 }
